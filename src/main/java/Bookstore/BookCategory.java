4ce6f020ef900e8bd506adcdd4a07cbd01e2234b
Robert Jaworowski <robert.jaworowski@interia.pl>
2018-05-05 12:00:46 +0200

    zaimplementowana strategia

---
+++ src/main/java/Bookstore/BookCategory.java
@@ -0,0 +1,12 @@
+package Bookstore;
+
+public class AuthorID {
+    private String authorIDName;
+    private int authorIDPriority;
+
+    AuthorID (String authorIDName, int authorIDPriority){
+        this.authorIDName = authorIDName;
+        this.authorIDPriority = authorIDPriority;
+
+    }
+}
